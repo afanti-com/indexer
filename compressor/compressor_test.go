5f6f0f2d1c0fe4ce127b6b6086bce580bddc99d8
Will <vvfriday@gmail.com>
2017-11-30 16:52:40 +0800

    add test & fixed bug

---
+++ compressor/compressor_test.go
@@ -0,0 +1,97 @@
+package compressor
+
+import (
+	"testing"
+	"fmt"
+	"os"
+	"bytes"
+)
+
+
+func TestEncodeAndDecode(t *testing.T) {
+
+	raw := []uint32{0, 32, 20003, 60006, 300009}
+	
+	fmt.Println("before encode:", raw)
+	
+	var b bytes.Buffer
+	var total_len uint = 0
+	for _, x := range raw {
+		len := Encode(&b, x)
+		total_len += len
+	}
+
+	fmt.Println("total encode len: ", total_len)
+	fmt.Println("encode result:", b.Bytes())
+	
+	// fmt.Println(len(b.Bytes()))
+	// fmt.Println(len(b.String()))
+
+	fout, err := os.OpenFile("compress.data", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		t.Errorf("open file error")
+	}
+	defer fout.Close()
+
+	n, err := b.WriteTo(fout)
+	if err != nil {
+		t.Errorf("writeto file error")
+	}
+
+	if total_len != uint(n) {
+		t.Errorf("write %d bytes not equals total_len %d", n, total_len)
+	}
+	
+	fmt.Println("writeto file bytes: ", n)
+
+	fin, err := os.Open("compress.data")
+	if err != nil {
+		t.Errorf("open file error")
+	}
+
+	var buf bytes.Buffer
+	bb := make([]byte, total_len)
+	m, err := fin.Read(bb)
+	if err != nil {
+		t.Errorf("readfrom file error")
+	}
+	if m != int(n) {
+		t.Errorf("read %d bytes from file not equals %d", m, n)
+	}
+	
+	m, err = buf.Write(bb)
+	if err != nil {
+		t.Errorf("buf write error")
+	}
+	
+	fmt.Println("buf read bytes: ", m)
+	fmt.Println(buf.Bytes())
+
+	result := make([]uint32, 0)
+	Decode(&buf, total_len, &result)
+
+	fmt.Println("decode result:", result)
+
+	if !isSliceEqual(raw, result) {
+		t.Errorf("decode result not equals raw data")
+	}
+
+}
+
+func isSliceEqual(a, b []uint32) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	if (a == nil) != (b == nil) {
+		return false
+	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ hashtable/hashtable_test.go
@@ -0,0 +1,62 @@
+package hashtable
+
+import (
+	_ "fmt"
+	"testing"
+)
+
+
+func TestHashtableInsertAndGet(t *testing.T) {
+	var size uint32 = 128
+	var word_id int32 = 0
+	var word_seq int32 = 0
+	
+	ht := new(HashTable)
+	ht.Init(size)
+
+	word_id = ht.Insert("hello", &word_seq)
+
+	if word_id != 0 || word_seq != 1 {
+		t.Errorf("hashtable insert uniq key error")
+	}
+	word_id = ht.Insert("world", &word_seq)
+	if word_id != 1 || word_seq != 2 {
+		t.Errorf("hashtable insert uniq key error")
+	}
+	
+	word_id = ht.Insert("hello", &word_seq)
+	if word_id != 0 || word_seq != 2 {
+		t.Errorf("hashtable insert repetitive key error")
+	}
+
+	word_id = ht.GetId("world")
+	if word_id != 1 {
+		t.Errorf("hashtable get key error")
+	}
+}
+
+func TestHashtableShow(t *testing.T) {
+	var size uint32 = 128
+	var word_seq int32 = 0
+	ht := new(HashTable)
+	ht.Init(size)
+	ht.Insert("hello", &word_seq)
+	ht.Insert("world", &word_seq)
+	ht.Insert("hello", &word_seq)
+
+	ht.Show()
+	ret := ht.Save("word_info.txt")
+	if ret != 0 {
+		t.Errorf("hashtable save word info error")
+	}
+	
+	new_ht := new(HashTable)
+	new_ht.Init(size)
+	ret = new_ht.Load("word_info.txt")
+	if ret != 0 {
+		t.Errorf("hashtable load word info error")
+	}
+	
+	new_ht.Show()
+}
+
